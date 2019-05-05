6f88970fe17b761cd087ded4c4fbca8b4c691256
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2019-05-05 23:55:34 +0300

    Update helper tests

--- spec/helper_functions.spec.php
+++ spec/helper_functions.spec.php
@@ -2,26 +2,20 @@
 
 use function CsvConverter\{DataHolder, CsvParser, JsonConverter};
 
-  describe('CsvParser', function(){
-    describe('()', function(){
-      it("creates instance of CsvParser", function() {
-        expect(CsvParser())->toBeA('object')->toBeAnInstanceOf('CsvConverter\CsvParser');
-      });
-    });
+describe('CsvParser()', function(){
+  it("creates instance of CsvParser", function() {
+    expect(CsvParser())->toBeA('object')->toBeAnInstanceOf('CsvConverter\CsvParser');
   });
+});
 
-  describe('DataHolder', function(){
-    describe('()', function(){
-      it("creates instance of DataHolder", function() {
-        expect(DataHolder())->toBeA('object')->toBeAnInstanceOf('CsvConverter\DataHolder');
-      });
-    });
+describe('DataHolder()', function(){
+  it("creates instance of DataHolder", function() {
+    expect(DataHolder())->toBeA('object')->toBeAnInstanceOf('CsvConverter\DataHolder');
   });
+});
 
-  describe('JsonConverter', function(){
-    describe('()', function(){
-      it("creates instance of JsonConverter", function() {
-        expect(JsonConverter())->toBeA('object')->toBeAnInstanceOf('CsvConverter\JsonConverter');
-      });
-    });
+describe('JsonConverter()', function(){
+  it("creates instance of JsonConverter", function() {
+    expect(JsonConverter())->toBeA('object')->toBeAnInstanceOf('CsvConverter\JsonConverter');
   });
+});
