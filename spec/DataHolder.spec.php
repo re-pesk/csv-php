40b996e45c33d876cb832df62e921ce57d4bba39
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2019-05-05 23:56:50 +0300

    Add DataHolder tests

---
+++ spec/DataHolder.spec.php
@@ -0,0 +1,36 @@
+<?php
+
+use function CsvConverter\{DataHolder, CsvParser, JsonConverter};
+
+describe("DataHolder", function(){
+  beforeEach(function() {
+    $this->dataHolder = DataHolder();
+  });
+  describe("()", function(){
+    it("returns object with class 'DataHolder'", function() {
+      expect($this->dataHolder)->toBeA('object')->toBeAnInstanceOf('CsvConverter\DataHolder');
+    });
+  });
+  describe("->addParser", function(){
+    it("adds csv parser to list of available parsers", function() {
+      $parser = CsvParser();
+      $this->dataHolder->addParser($parser);
+      expect($this->dataHolder->hasParser($parser->inputType()))
+        ->toBeA('boolean')
+        ->toEqual(true);
+    });
+  });
+  describe("->removeParser", function(){
+    it("remove csv parser from list of available parsers", function() {
+      $parser = CsvParser();
+      $this->dataHolder->addParser($parser);
+      expect($this->dataHolder->hasParser($parser->inputType()))
+        ->toBeA('boolean')
+        ->toEqual(true);
+      $this->dataHolder->removeParser($parser->inputType());
+      expect($this->dataHolder->hasParser($parser->inputType()))
+        ->toBeA('boolean')
+        ->toEqual(false);
+    });
+  });
+});
