db4614f1f5398a81974a3980b2aa03e849a18c70
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-05-20 14:37:56 +0300

    Fix error

--- index.php
+++ index.php
@@ -6,71 +6,94 @@ use function CsvConverter\{DataHolder, CsvParser, JsonConverter};
 use CsvConverter\JsonConverter;
 
 $jsonConverter = new JsonConverter();
-echo "\$jsonConverter->dataType() === '{$jsonConverter->dataType()}'\n";
+echo "\$jsonConverter->dataType() === '{$jsonConverter->dataType()}'\n\n";
 
 $csvParser = CsvParser()->parameters([ 'hasHeader' => true, 'convertToNull' => true, 'ignoreInvalidChars' => true ]);
 
 $csvList = [
-  1 => "a,b,c\r\nzzz,\",\r\n\"\r\n2,,",
   0 => "a,b,c\r\nzzz,,\",\r\n\"2,,",
+  1 => "a,b,c\r\nzzz,\",\r\n\"\r\n2,,",
   2 => "a,b,c\r\nzzz,,,\",\r\n\"\r\n2,,",
   3 => "a,b,c\r\nzzz,,\",\r\n\"\r\n2,",
   4 => "a,b,c\r\nzzz,, \"\n\"\r\n2,,",
   5 => "a,b,c\r\nzzz,,\"\r\"\n\r\n2,,",
 ];
 
-array_walk($csvList, function($csv, $i) use ($csvParser) {
-  $i++;
-  echo "Test #{$i}:\n\n";
-  echo "Input:\n\n"; var_export($csv); echo "\n\n";
-  try {
-    $str = var_export($csvParser->makeDataTree($csv), true);
-    echo "Data Tree:\n\n", $str, "\n\n";
-  } catch (Exception $e) {
-    echo "Error:\n\n", $e->getMessage(), "\n\n";
-  }
-});
+array_walk(
+    $csvList,
+    function ($csv, $i) use ($csvParser) {
+        $i++;
+        echo "Test #{$i}:\n\n";
+        echo "Input:\n\n";
+        var_export($csv);
+        echo "\n\n";
+        try {
+            $str = var_export($csvParser->makeDataTree($csv), true);
+            echo "Data Tree:\n\n", $str, "\n\n";
+        } catch (Exception $e) {
+            echo "Error:\n\n", $e->getMessage(), "\n\n";
+        }
+    }
+);
+
+$csvParser = CsvParser()->hasHeader(true)->convertToNull(true);
+$csvParser->parameters = [ 'hasHeader' => true ];
+$dataHolder = DataHolder($csvParser, JsonConverter());
+echo '$dataHolder->hasParser($csvParser->dataType()) === ', $dataHolder->hasParser($csvParser->dataType()), "\n";
+// $dataHolder->removeParser('csv');
+// echo '$dataHolder->hasParser($csvParser->dataType()) ===', $dataHolder->hasParser($csvParser->dataType());
+
+$dataHolder->csv = 'a,b,"x""y"';
+echo '$dataHolder->dataTree === ';
+var_export($dataHolder->dataTree);
+echo "\n\n";
+echo '$dataHolder->json ===', $dataHolder->json, "\n";
+echo '$dataHolder->json() ===', $dataHolder->json(), "\n";
+
+echo "\n\n";
+echo "Data Tree:\n\n";
+var_export($dataHolder->dataTree);
+echo "\n\n";
+echo "JSON:\n\n";
+var_dump($dataHolder->json); //    echo "\n\n"; $dataHolder->json(JSON_PRETTY_PRINT)
+
+$parser = CsvParser()->hasHeader(true);
+echo '$parser->hasHeader: ', $parser->hasHeader ? 'true' : 'false', "\n\n";
+echo '$parser->convertToNull: ', $parser->convertToNull ? 'true' : 'false', "\n\n";
+echo '$parser->ignoreInvalidChars: ', $parser->ignoreInvalidChars ? 'true' : 'false', "\n\n";
+
+$parser->parameters = [ 'hasHeader' => true, 'ignoreInvalidChars' => true ];
+
+$parser->hasHeader = true;
+$parser->convertToNull = true;
 
 $csv = 'field_name_1,"Field
 Name 2",\rfield_name_3\n 
 aaa,"b 
-,bb","ccc""ddd"
+,bb","ccc""ddd"\r
 zzz,,""
 1,2.2,
 ,3,
 ';
 
 echo "\n";
-echo "Input:\n\n"; var_export($csv); echo "\n\n";
-
-$csvParser = CsvParser()->hasHeader(true)->convertToNull(true);
-$csvParser->parameters = [ 'hasHeader' => true ];
-$dataHolder = DataHolder($csvParser, JsonConverter());
-echo '$dataHolder->hasParser($csvParser->dataType()) ===', $dataHolder->hasParser($csvParser->dataType());
-// $dataHolder->removeParser('csv');
-// echo '$dataHolder->hasParser($csvParser->dataType()) ===', $dataHolder->hasParser($csvParser->dataType());
+echo "Input:\n\n";
+var_export($csv);
+echo "\n\n";
 
 
-$dataHolder->csv = 'a,b,"x""y"';
-echo '$dataHolder->dataTree ==='; var_export($dataHolder->dataTree);
-echo '$dataHolder->json ===', $dataHolder->json, "\n";
-echo '$dataHolder->json() ===', $dataHolder->json(), "\n";
+echo "-------------------\n";
 
+$csvParser->parameters = [ 'hasHeader' => true, 'ignoreInvalidChars' => true ];
 $records = $csvParser->makeRecords($csv);
 
-echo "Records:\n\n"; array_walk($records, function($record){
-  var_dump($record);
-}); echo "\n\n";
-echo "Data Tree:\n\n"; var_export($dataHolder->dataTree); echo "\n\n";
-echo "JSON:\n\n";      var_dump($dataHolder->json); //    echo "\n\n"; $dataHolder->json(JSON_PRETTY_PRINT)
+echo "Records:\n\n"; array_walk(
+    $records,
+    function ($record) {
+        var_dump($record);
+    }
+);
 
-$parser = CsvParser()->hasHeader(true);
-echo '$parser->hasHeader: ', $parser->hasHeader ? 'true' : 'false', "\n\n";
-echo '$parser->convertToNull: ', $parser->convertToNull ? 'true' : 'false', "\n\n";
-
-$parser->hasHeader = true;
-$parser->convertToNull = true;
-
-echo "Data Tree:\n\n"; var_dump($parser->makeDataTree($csv)); echo "\n\n";
-
-
+echo "Data Tree:\n\n";
+var_dump($parser->makeDataTree($csv));
+echo "\n\n";
